81b99e43b0a000045d20b039530d05b5f7fe18f2
Hector Rodriguez <syr112107@github.com>
2025-04-03 13:52:54 -0500

    Trigger: run clean workflow

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -5,3 +5,5 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello Updated from Lambda VScode')
     }# final trigger test
+
+# force trigger after fresh deploy reset
