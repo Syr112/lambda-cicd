142b33e6464a13af5a34d753f291b6f9a3c19ad5
Hector Rodriguez <syr112107@github.com>
2025-04-03 12:56:06 -0500

    Trigger workflow by touching lambda code

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello updated Lambda from VSCode')
+        'body': json.dumps('Hello Updated from Lambda VScode')
     }
